4a907685c586f1272bb153a4dd4f1b1500bbaeb4
Cherish Rikardo <d13330701@thu.edu.tw>
2024-09-25 17:03:47 +0800

    add somtingggg

---
+++ 3_hello_name_input.py
@@ -0,0 +1,13 @@
+#
+# Cherish
+# Take name as an input then print Hello name
+#
+
+# 1. Input
+n = input('Name: ') 
+m = input('Age: ')
+
+# 2. Process
+
+# 3. Output
+print(f'Hello {n} your age is {m}!')
---
+++ 4_add_2_numbers.py
@@ -0,0 +1,16 @@
+#
+# Cherish
+# Take 2 numbers as input, add them, adn print the result
+#
+
+# 1. Input
+n1 = int(input('Number 1: '))
+n2 = int(input('Number 2: '))
+
+# 2. Process
+sum = n1 + n2
+multiply = n1*n2
+
+# 3. Output
+print(f'Sum     : {sum}')
+print(f'Multiply: {multiply}')
